0bb8be91fbaf4824d9a82847f738b5a66517b417
Anand Keshav <95139377+keshavanand1717@users.noreply.github.com>
2023-06-15 17:00:16 +0530

    Sign-Up

--- core/views.py
+++ core/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .import forms
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def sign_up(request):
+    form = forms.SignUpForm()
+    return render(request, 'sign_up.html',{
+        'form': form
+    })
